3521b2b727dffefd25e5bb074c4a6118bc9dd00b
Demitri <gremiodemke@yahoo.es>
2021-08-12 19:29:18 +0200

    Code changes to show more easily variable content with xdebug

--- application/src/Controller/DefaultController.php
+++ application/src/Controller/DefaultController.php
@@ -12,9 +12,22 @@ final class DefaultController extends AbstractController
     {
         $variable= 'hopla';
         $variable1= 'hopla';
-        $variable13= 'hopla';
-var_dump($variable);
+        $variable13= $this->doWhatever(30);
 
-        return $this->render('Default\index.html.twig');
+
+        return $this->render('Default\index.html.twig', ['v' => $variable,'v2' =>  $variable1,'v3' => $variable13]);
+    }
+
+    private function doWhatever(int $lenght): string
+    {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $randomString = '';
+
+        for ($i = 0; $i < $lenght; $i++) {
+            $index = random_int(0, strlen($characters) - 1);
+            $randomString .= $characters[$index];
+        }
+
+        return $randomString;
     }
 }
